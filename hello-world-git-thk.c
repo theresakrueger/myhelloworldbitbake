fd4abea3c4447be896c0705ba505b5b1a7592f1d
test <test@test.com>
2024-03-09 16:34:22 +0100

    branch1 first commint

--- hello-world-git-thk.c
+++ hello-world-git-thk.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
 int main(void) {
-	printf("Hello, World from an application cloned from Github thk!\r\n");
+	printf("Hello, World from an application cloned from Github thk branch1!\r\n");
 	return 0;
 }
